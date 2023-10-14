050a464b67b9bbcd4fc04d09c5bd7a769fb621b4
Biohacker0 <50107470+biohacker0@users.noreply.github.com>
2023-10-14 16:15:32 +0530

    Time: 10 ms (70.92%), Space: 11.5 MB (13.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+        map<int,int> myMap;
+
+        for(int i=0;i<nums.size() ; i++){
+            int val = target-nums[i];
+            if(myMap.count(val)){
+                return {myMap[val] , i };
             }
+            myMap[nums[i]]=i;
         }
-
-        return ans;
-
+        
+  return{-1 , -1};
     }
 };
