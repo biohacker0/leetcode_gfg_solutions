8f2be85feccde2a79d916c50f0dfdf87934abd9b
Biohacker0 <50107470+biohacker0@users.noreply.github.com>
2023-10-16 19:29:14 +0530

    Time: 76 ms (5.08%) | Memory: 43 MB (5.40%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int curWindow = 0;
+        int minLength = INT_MAX; // Initialize minLength to a large value.
+        int sum = 0;
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+
+            while (sum >= target) {
+                minLength = min(minLength, i - curWindow + 1);
+                sum -= nums[curWindow];
+                curWindow++;
+            }
+
+            if (!map.count(sum)) {
+                map[sum] = i;
+            }
+        }
+
+        return (minLength == INT_MAX) ? 0 : minLength;
+    }
+};
