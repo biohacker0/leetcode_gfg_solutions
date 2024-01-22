7b578a254ef1d951daf31c6236024df120b10e15
Biohacker0 <50107470+biohacker0@users.noreply.github.com>
2024-01-23 04:45:43 +0530

    Time: 48 ms (80.43%) | Memory: 48.5 MB (5.12%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let filteredArr =[]
+    for(let items in arr){
+        let result = fn(arr[items],Number(items))
+        if(result!=0 || result==true)filteredArr.push(arr[items]);
+    }
+    return filteredArr
+};
